7c6f515cf95e863ab5a3ce032feb3c632c99b24c
Tony Zhang <nz97@cornell.edu>
2023-11-13 21:52:36 -0800

    sloc scripts for evaluation

---
+++ messageInvariantPrototypes/evaluation/all_sloc.py
@@ -0,0 +1,35 @@
+import csv
+from sloc import count_sloc_between
+
+CSV_FILE = "protocols.csv"
+ROOT_PATH = "/Users/nudzhang/Documents/UMich2023sp/linear-dist.nosync/messageInvariantPrototypes"
+OUTPUT_PATH = "/Users/nudzhang/Documents/UMich2023sp/linear-dist.nosync/messageInvariantPrototypes/evaluation/sloc.csv"
+
+def analyze_protocol(file_path):
+    with open(file_path, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        res = []
+        res.append("protocol,total_sync_proof,total_async_proof")
+
+        for row in csv_reader:
+            protocol = row[0]
+            async_roof_file_path = f"{ROOT_PATH}/{protocol}/automate/applicationProof.dfy"
+            sync_proof_file_path = f"{ROOT_PATH}/{protocol}/centralized/applicationProof.dfy"
+            async_sloc = count_sloc_between(async_roof_file_path, 1, 1000000)
+            sync_sloc = count_sloc_between(sync_proof_file_path, 1, 1000000)
+
+            line = f"{protocol},{sync_sloc},{async_sloc}"
+            res.append(line)
+    write_to_file(OUTPUT_PATH, "\n".join(res))
+
+
+def write_to_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
+
+
+def main():
+    analyze_protocol(CSV_FILE)
+
+if __name__ == "__main__":
+    main()
