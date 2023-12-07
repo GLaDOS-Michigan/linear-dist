dc174a19e33a8f2b449e664a6f183d88a7408eb9
Tony Zhang <nz97@cornell.edu>
2023-12-07 12:17:39 -0800

    count sync spec sloc

--- messageInvariantPrototypes/evaluation/all_sloc.py
+++ messageInvariantPrototypes/evaluation/all_sloc.py
@@ -9,16 +9,27 @@ def analyze_protocol(file_path):
     with open(file_path, 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
         res = []
-        res.append("protocol,total_sync_proof,total_async_proof")
+        res.append("protocol,sync_spec,total_sync_proof,total_async_proof")
 
         for row in csv_reader:
             protocol = row[0]
+
             async_roof_file_path = f"{ROOT_PATH}/{protocol}/automate/applicationProof.dfy"
             sync_proof_file_path = f"{ROOT_PATH}/{protocol}/centralized/applicationProof.dfy"
             async_sloc = count_sloc_between(async_roof_file_path, 1, 1000000)
             sync_sloc = count_sloc_between(sync_proof_file_path, 1, 1000000)
 
-            line = f"{protocol},{sync_sloc},{async_sloc}"
+            spec_file_paths = [
+                f"{ROOT_PATH}/{protocol}/hosts.dfy",
+                f"{ROOT_PATH}/{protocol}/types.dfy",
+                f"{ROOT_PATH}/{protocol}/centralized/spec.dfy",
+                f"{ROOT_PATH}/{protocol}/centralized/system.dfy"
+            ]
+            spec_sloc = 0
+            for fp in spec_file_paths:
+                spec_sloc += count_sloc_between(fp, 1, 1000000)
+
+            line = f"{protocol},{spec_sloc},{sync_sloc},{async_sloc}"
             res.append(line)
     write_to_file(OUTPUT_PATH, "\n".join(res))
 
